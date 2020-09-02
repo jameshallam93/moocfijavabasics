f4704cb74a0f570f57968a0af1e29203b7c73fac
jameshallam93 <63243789+jameshallam93@users.noreply.github.com>
2020-09-02 16:49:22 +0100

    Add files via upload

---
+++ Part5/part05-Part05_09.HealthStation/src/main/java/HealthStation.java
@@ -0,0 +1,26 @@
+
+public class HealthStation {
+    
+    private int numberOfWeighs;
+
+    public HealthStation() {
+        this.numberOfWeighs = 0;
+    }
+
+
+    public int weigh(Person person) {
+        // return the weight of the person passed as the parameter
+        numberOfWeighs = numberOfWeighs + 1;
+        return person.getWeight();
+
+        
+    }
+    public void feed(Person person){
+        person.setWeight(person.getWeight() + 1);
+        
+    }
+    public int weighings(){
+        return numberOfWeighs;
+    }
+
+}
